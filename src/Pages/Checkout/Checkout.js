2abaedfc1e7da387885d498024435a1741382882
AF Tamim <itamim345@gmail.com>
2022-11-22 23:59:50 +0530

    Added a Modal

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 export default function Checkout() {
+  //For Modal
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className='container py-2'>
       <h2 className='text-center text-danger text-decoration-underline py-2'>Please Checkout your Booking</h2>
       <div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Form className="w-50 mx-auto">
       <Form.Group className="mb-3" controlId="formBasicTransactionId">
         <Form.Label>Transaction ID</Form.Label>
@@ -31,6 +48,10 @@ export default function Checkout() {
         Submit
       </Button>
     </Form>
+    
+
+    
+
       </div>
     </div>
   );
