ba1a43975e993bfe4a2ade959d4ba40ea76649da
AF Tamim <itamim345@gmail.com>
2022-11-23 00:16:37 +0530

    Modified Modal & Checkout Form

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -11,13 +11,33 @@ export default function Checkout() {
     <div className='container py-2'>
       <h2 className='text-center text-danger text-decoration-underline py-2'>Please Checkout your Booking</h2>
       <div>
+
+      <div className="w-50 mx-auto">
+        <Button variant="primary" onClick={handleShow} className="w-100">
+          Get The Payment Details To Pay
+        </Button>
+      </div>
+
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title className='text-success'>Pay Using Any Payment Method</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <h6>Paytm: 9797979797@paytm</h6>
+          <hr></hr>
+          <h6>PhonePe: 9797979797@ybl</h6>
+          <hr></hr>
+          <h6>Gpay: 9797979797@gpay</h6>
+          <hr></hr>
+          <h5>Bank Details:- State Bank Of India
+          </h5>
+          <p>
+            Name: Physiocare Private Limited,
+            Account NO: 9797979797979, IFSC: SBIN000111
+          </p>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="danger" onClick={handleClose}>
             Close
           </Button>
         </Modal.Footer>
@@ -30,17 +50,17 @@ export default function Checkout() {
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPaymentType">
-        <Form.Label>Payment Via</Form.Label>
-        <Form.Control type="text" placeholder="Paytm/PhonePe/GPay/Others" />
+        <Form.Label>Payment Method</Form.Label>
+        <Form.Control type="text" placeholder="Paytm/PhonePe/GPay/Bank Transfer" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicContactNumber">
         <Form.Label>Contact Number</Form.Label>
-        <Form.Control type="text" placeholder="Enter Yor Contact Number" />
+        <Form.Control type="text" placeholder="Enter Your Contact Number" />
       </Form.Group>
 
       <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Default file input example</Form.Label>
+        <Form.Label>Attach Your Payment Proof</Form.Label>
         <Form.Control type="file" />
       </Form.Group>
       
