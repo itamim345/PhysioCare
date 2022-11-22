93e41314b54270a8c20d295b6e06cfd88d574b73
AF Tamim <itamim345@gmail.com>
2022-11-22 23:42:40 +0530

    Added Basic Checkout Form

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 export default function Checkout() {
   return (
-    <div>
+    <div className='container py-5'>
       <h2>Please Checkout your Booking</h2>
+      <div>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicTransactionId">
+        <Form.Label>Transaction ID</Form.Label>
+        <Form.Control type="email" placeholder="Enter your transaction ID" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPaymentType">
+        <Form.Label>Payment Via</Form.Label>
+        <Form.Control type="text" placeholder="Paytm/PhonePe/GPay/Others" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicContactNumber">
+        <Form.Label>Contact Number</Form.Label>
+        <Form.Control type="text" placeholder="Enter Yor Contact Number" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      </div>
     </div>
   );
 }
