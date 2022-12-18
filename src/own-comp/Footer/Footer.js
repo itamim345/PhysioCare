dab455412494fccf3012ad4a1414ba2ee8a2b123
AF Tamim <itamim345@gmail.com>
2022-12-18 21:02:02 +0530

    Added Icons in Footer

--- src/own-comp/Footer/Footer.js
+++ src/own-comp/Footer/Footer.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className='bg-dark text-white p-3 text-center'>
-            <div className='container'>
-                <p>&copy; 2022 All Right Reserved By &nbsp;
-                    <a href="/">PhysioCare</a>
-                </p>
-                <p>
-                    <a href='/'>Facebook</a>
-                    &nbsp;&nbsp;
-                    <a href='/'>Instagram</a>
-                    &nbsp;&nbsp;
-                    <a href='/'>Twitter</a>
-                </p>
-            </div>
-        </footer>
+      <footer className="bg-dark text-white p-3 text-center">
+        <div className="container">
+          <p>
+            &copy; 2022 All Right Reserved By &nbsp;
+            <a href="/">PhysioCare</a>
+          </p>
+          <p>
+            <i className="ri-facebook-circle-fill"></i>
+            &nbsp;&nbsp;
+            <i className="ri-twitter-fill"></i>
+            &nbsp;&nbsp;
+            <i className="ri-instagram-fill"></i>
+          </p>
+        </div>
+      </footer>
     );
 }
