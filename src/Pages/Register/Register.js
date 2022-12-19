339da9015579c3b1f4ae4bd303859f41c8ec1492
AF Tamim <itamim345@gmail.com>
2022-12-19 21:18:25 +0530

    Removed extra line from register form

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -47,9 +47,6 @@ export default function Register() {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" name="email" placeholder="Enter email" required/>
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
